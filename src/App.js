35e2a631ff716ac912ea352e63a3a59a56ca7ad1
Lilia Mabel Borrazás <lilia.borrazas@ultimasoftware.com>
2019-10-03 16:40:45 +0200

    create form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './bootstrap.min.css';
+import Header from './components/Header';
+import NuevaCita from './components/NuevaCita';
 
-function App() {
-  return (
-    <div className="App">
-     
+class App extends Component {
+  state = {
+
+  }
+  render() {
+  return(
+    <div className="container">
+      <Header
+        titulo='Administrador Pacientes Veterinaria'
+        />
+        <div className="row">
+          <div className="col-md-10 mx-auto">
+         <NuevaCita/> 
+
+          </div>
+        </div>
     </div>
   );
 }
-
+}
 export default App;
