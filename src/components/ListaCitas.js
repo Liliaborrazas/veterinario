97c3351d997b308a6aa50ed093249c4f24166b3b
Lilia Mabel Borrazás <lilia.borrazas@ultimasoftware.com>
2019-10-07 10:14:45 +0200

    mensaje de mostrar citas ok

--- src/components/ListaCitas.js
+++ src/components/ListaCitas.js
@@ -3,11 +3,12 @@ import Cita from './Cita';
 
 
 export default function ListaCitas({citas, eliminarCita}){
+    const mensaje = Object.keys(citas).length === 0 ? 'No hay citas' : 'Administra las citas aquí';
     return(
         <>
         <div className="card mt-2 py-5">
             <div className="card-body">
-                <h2 className="card-title text-center">Administra las citas aquí</h2>
+                <h2 className="card-title text-center">{mensaje}</h2>
                 <div className="lista-citas">
                    {citas.map(cita=>(
                        <Cita
